d7239655cf1cd04edd47fe8343369cf7a650096d
CheezeLab <cheezelab@kakao.com>
2022-04-11 11:34:09 +0900

    Revert "[test] next text, change project setting and test"
    This reverts commit 085f2853a92b1e57a6b53a0258e354fb039d7796.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import styles from '@/styles/Home.module.css';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
   return (
@@ -19,10 +20,6 @@ const Home: NextPage = () => {
         <p className={styles.description}>
           Get started by editing <code className={styles.code}>pages/index.tsx</code>
         </p>
-
-        <div className="-mt-10">
-          <h1 className="text-3xl font-bold underline">Hello Next.js world!!</h1>
-        </div>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
@@ -49,6 +46,19 @@ const Home: NextPage = () => {
           </a>
         </div>
       </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <span className={styles.logo}>
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          </span>
+        </a>
+      </footer>
     </div>
   );
 };
